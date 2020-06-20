0b0ebaf167f1ea987255ba180596291c9ab55a65
Adam Sears <formlesstree4@tenfoldstudios.com>
2020-06-20 16:02:53 -0500

    AS | Initial commit of all source code

---
+++ source/Bot/Models/PlayerContext.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+
+public sealed class PlayerContext : DbContext
+{
+    
+
+}
---
+++ source/Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
