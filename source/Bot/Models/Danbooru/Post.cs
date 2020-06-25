2b450cb4fe393211b080ffed416db82a64e87aa2
Adam Sears <formlesstree4@tenfoldstudios.com>
2020-06-25 00:22:49 -0500

    I made every damn field nullable in Post 'cause danbooru's API is mean

--- source/Bot/Models/Danbooru/Post.cs
+++ source/Bot/Models/Danbooru/Post.cs
@@ -26,13 +26,13 @@ namespace Bot.Models.Danbooru
         ///     Gets or sets the User ID that uploaded this <see cref="Post"/>
         /// </summary>
         [JsonProperty("uploader_id")]
-        public int UploaderId { get; set; }
+        public int? UploaderId { get; set; }
 
         /// <summary>
         ///     Gets or sets the Score of this <see cref="Post"/>
         /// </summary>
         [JsonProperty("score")]
-        public int Score { get; set; }
+        public int? Score { get; set; }
 
         /// <summary>
         ///     Gets or sets the source of this <see cref="Post"/>
@@ -54,44 +54,44 @@ namespace Bot.Models.Danbooru
 
 
         public string rating { get; set; }
-        public int image_width { get; set; }
-        public int image_height { get; set; }
+        public int? image_width { get; set; }
+        public int? image_height { get; set; }
         public string tag_string { get; set; }
-        public bool is_note_locked { get; set; }
-        public int fav_count { get; set; }
+        public bool? is_note_locked { get; set; }
+        public int? fav_count { get; set; }
         public string file_ext { get; set; }
         public DateTime? last_noted_at { get; set; }
-        public bool is_rating_locked { get; set; }
+        public bool? is_rating_locked { get; set; }
         public int? parent_id { get; set; }
-        public bool has_children { get; set; }
+        public bool? has_children { get; set; }
         public int? approver_id { get; set; }
-        public int tag_count_general { get; set; }
-        public int tag_count_artist { get; set; }
-        public int tag_count_character { get; set; }
-        public int tag_count_copyright { get; set; }
-        public int file_size { get; set; }
-        public bool is_status_locked { get; set; }
+        public int? tag_count_general { get; set; }
+        public int? tag_count_artist { get; set; }
+        public int? tag_count_character { get; set; }
+        public int? tag_count_copyright { get; set; }
+        public int? file_size { get; set; }
+        public bool? is_status_locked { get; set; }
         public string fav_string { get; set; }
         public string pool_string { get; set; }
-        public int up_score { get; set; }
-        public int down_score { get; set; }
-        public bool is_pending { get; set; }
-        public bool is_flagged { get; set; }
-        public bool is_deleted { get; set; }
-        public int tag_count { get; set; }
-        public DateTime updated_at { get; set; }
-        public bool is_banned { get; set; }
+        public int? up_score { get; set; }
+        public int? down_score { get; set; }
+        public bool? is_pending { get; set; }
+        public bool? is_flagged { get; set; }
+        public bool? is_deleted { get; set; }
+        public int? tag_count { get; set; }
+        public DateTime? updated_at { get; set; }
+        public bool? is_banned { get; set; }
         public int? pixiv_id { get; set; }
         public DateTime? last_commented_at { get; set; }
-        public bool has_active_children { get; set; }
-        public int bit_flags { get; set; }
+        public bool? has_active_children { get; set; }
+        public int? bit_flags { get; set; }
         public string uploader_name { get; set; }
-        public bool has_large { get; set; }
+        public bool? has_large { get; set; }
         public string tag_string_artist { get; set; }
         public string tag_string_character { get; set; }
         public string tag_string_copyright { get; set; }
         public string tag_string_general { get; set; }
-        public bool has_visible_children { get; set; }
+        public bool? has_visible_children { get; set; }
         public string file_url { get; set; }
         public string large_file_url { get; set; }
         public string preview_file_url { get; set; }
