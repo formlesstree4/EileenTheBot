f902f2d1c570841f57483cc1b09c26a9eb75a15f
Adam Sears <formlesstree4@tenfoldstudios.com>
2020-07-06 20:49:05 -0500

    Per #4 - Simplify the various booru services and consolidate into a base class with slight differences

--- source/Bot/Services/Booru/DanbooruService.cs
+++ source/Bot/Services/Booru/DanbooruService.cs
@@ -1,14 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using Bot.Models.Danbooru;
-using Newtonsoft.Json;
 
 namespace Bot.Services.Booru
 {
@@ -18,76 +9,16 @@ namespace Bot.Services.Booru
     /// <summary>
     ///     A simplified querying engine for Danbooru.
     /// </summary>
-    public sealed class DanbooruService : IBooruProvider<Post>
+    public sealed class Danbooru : BooruService<Post[], Post>
     {
+        public Danbooru(CredentialsService credentials) : base(credentials) { }
 
-        private readonly HttpClient _clientAsync;
-        private readonly string _user;
-        private readonly string _apiKey;
+        protected override IEnumerable<Post> ConvertResponseAsEnumerable(Post[] response) => response;
 
+        protected override string GetCredentialsKey() => "danbooru";
 
-
-        /// <summary>
-        ///     Creates a new instance of the <see cref="DanbooruService"/>.
-        /// </summary>1
-        public DanbooruService(CredentialsService credentials)
-        {
-            var configuration = credentials.Credentials.First(c => c.Name.Equals("danbooru"));
-            _user = configuration.Username;
-            _apiKey = configuration.ApiKey;
-            _clientAsync = new HttpClient();
-            _clientAsync.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _clientAsync.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_user}:{_apiKey}")));
-        }
-
-
-
-        /// <summary>
-        ///     Asynchronously performs a search against the Danbooru API.
-        /// </summary>
-        /// <param name="limit">The number of images that can be returned</param>
-        /// <param name="page">The page offset for pagination support</param>
-        /// <param name="searchTags">A string array of tags that should be searched</param>
-        /// <returns>A collection of <see cref="Post"/> objects</returns>
-        public async Task<IEnumerable<Post>> SearchAsync(int limit, int page, params string[] searchTags)
-        {
-            var tags = WebUtility.UrlEncode(string.Join(" ", searchTags));
-            var url = $"https://danbooru.donmai.us/posts.json?limit={limit}&page={page}&tags={tags}";
-            var resp = await _clientAsync.GetAsync(url);
-            var respString = await resp.Content.ReadAsStringAsync();
-            var posts = JsonConvert.DeserializeObject<Post[]>(respString);
-            return posts;
-        }
-
-        /// <summary>
-        ///     Asynchronously performs a search against the Danbooru API using the 'random' endpoint
-        /// </summary>
-        /// <param name="searchTags">A string array of tags that should be searched</param>
-        /// <returns><see cref="Post"/></returns>
-        public async Task<Post> SearchRandom(params string[] searchTags)
-        {
-            var tags = WebUtility.UrlEncode(string.Join(" ", searchTags));
-            var url = $"https://danbooru.donmai.us/posts/random.json?tags={tags}";
-            var resp = await _clientAsync.GetAsync(url);
-            var respString = await resp.Content.ReadAsStringAsync();
-            var post = JsonConvert.DeserializeObject<Post>(respString);
-            return post;
-        }
-
-        /// <summary>
-        ///     Asynchronously downloads a post.
-        /// </summary>
-        /// <param name="p">The <see cref="Post"/> to download</param>
-        /// <returns><see cref="MemoryStream"/></returns>
-        public async Task<HttpResponseMessage> DownloadPostAsync(Post p)
-        {
-            return await _clientAsync.GetAsync(p.GetDownloadUrl());
-        }
-
+        protected override string GetSearchString(int limit, int page, string searchTags) => $"https://danbooru.donmai.us/posts.json?limit={limit}&page={page}&tags={searchTags}";
     }
-
-#pragma warning disable IDE1006 // Naming Styles
 
 
 }
--- source/Bot/Services/Booru/e621Service.cs
+++ source/Bot/Services/Booru/e621Service.cs
@@ -1,62 +1,19 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using Bot.Models.e621;
-using Newtonsoft.Json;
 
 namespace Bot.Services.Booru
 {
 
-    /// <summary>
-    ///     A simplified querying engine for e621.
-    /// </summary>
-    public sealed class e621Service : IBooruProvider<Post>
+    public sealed class e621 : BooruService<PostResponse, Post>
     {
+        public e621(CredentialsService credentials) : base(credentials) { }
 
-        private readonly HttpClient _clientAsync;
-        private readonly string _user;
-        private readonly string _apiKey;
+        protected override IEnumerable<Post> ConvertResponseAsEnumerable(PostResponse response) => response.Posts;
 
+        protected override string GetCredentialsKey() => "e621";
 
-
-        /// <summary>
-        ///     Creates a new instance of the <see cref="e621Service"/>.
-        /// </summary>
-        public e621Service(CredentialsService credentials)
-        {
-            var configuration = credentials.Credentials.First(c => c.Name.Equals("e621"));
-            var userAgent = new ProductHeaderValue($"The-Erector-by-{configuration.Username}", "1.0");
-            _user = configuration.Username;
-            _apiKey = configuration.ApiKey;
-            _clientAsync = new HttpClient();
-            _clientAsync.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _clientAsync.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(userAgent));
-            _clientAsync.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_user}:{_apiKey}")));
-        }
-
-        public async Task<IEnumerable<Post>> SearchAsync(int limit, int page, params string[] searchTags)
-        {
-            var tags = WebUtility.UrlEncode(string.Join(" ", searchTags));
-            var url = $"https://e621.net/posts.json?limit={limit}&page={page}&tags={tags}";
-            var resp = await _clientAsync.GetAsync(url);
-            var respString = await resp.Content.ReadAsStringAsync();
-            Console.WriteLine(respString);
-            var posts = JsonConvert.DeserializeObject<PostResponse>(respString);
-            return posts.Posts;
-        }
-
-        public async Task<Post> SearchRandom(params string[] searchTags)
-        {
-            var tags = new List<string>(searchTags);
-            tags.Add("order:random");
-            return (await SearchAsync(1, 1, tags.ToArray())).FirstOrDefault();
-        }
+        protected override string GetSearchString(int limit, int page, string searchTags) => $"https://e621.net/posts.json?limit={limit}&page={page}&tags={searchTags}";
+        
     }
 
 }
