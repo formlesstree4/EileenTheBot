d89cd44518e252179e0c476485d3725f4327b6c3
Gnbrkm41 <ganbarukamo@gmail.com>
2020-08-05 21:50:11 +0900

    owo uwu what's dis

---
+++ source/Bot/Modules/OwoModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Bot.Modules
+{
+    public class OwoModule : ModuleBase<SocketCommandContext>
+    {
+        private static readonly string[] s_faces = { "(\x30FB`\x03C9\x00B4\x30FB)", ";;w;;", "owo", "UwU", ">w<", "^w^", "◕w◕", "(⁄ʘ⁄⁄ω⁄⁄ʘ⁄)♡", "*𝓌𝒶𝓉𝓈 𝒹𝒾𝓈?*ღ(O꒳Oღ)", "( ͡o ꒳ ͡o )*𝔫𝔬𝔱𝔦𝔠𝔢𝔰 𝔟𝔲𝔩𝔤𝔢*" };
+
+        private readonly Random _rng = new Random();
+
+        [Command("owo")]
+        public async Task Owoify([Remainder] string text)
+        {
+            await Context.Message.DeleteAsync().ConfigureAwait(false);
+
+            var output = OwoifyText(text);
+
+            await ReplyAsync(output).ConfigureAwait(false);
+        }
+
+        public string OwoifyText(string text)
+        {
+            switch (text.ToLower()) // Because case sensitivity
+            {
+                case "owo uwu owo":
+                    return "human trafficking"; // https://cdn.discordapp.com/attachments/571064920808882176/573230351284043776/unknown.png
+                case "owo uwu owo uwu":
+                    return "no one can afford it";
+                case "owo uwu owo uwu owo":
+                    return "no one can afford to hire a person";
+                case "owo uwu owo uwu owo uwu":
+                    return "no one can afford to be lost";
+                case "owo uwu owo uwu owo uwu owo":
+                    return "no one can afford to buy a man";
+                case "owo uwu owo uwu owo uwu owo uwu":
+                    return "a traveler does not want to be lonely";
+                case "owo uwu owo uwu owo uwu owo uwu owo":
+                    return "no one can afford to be a victim of poverty";
+                case "owo uwu owo uwu owo uwu owo uwu owo uwu":
+                    return "no one can afford to be lonely";
+                case "owo uwu owo uwu owo uwu owo uwu owo uwu owo":
+                    return "a person who does not want to be able to find himself in a state of emergency does not have to be a victim of human suffering";
+            }
+
+            var result = Regex.Replace(text, "[rl]", "w"); // Replace 'r' or 'l' with 'w'
+            result = Regex.Replace(result, "[RL]", "W"); // Same as above, but for upper-case
+            result = Regex.Replace(result, "(n)([aeiouAEIOU])", "$1y$2"); // Replace 'n' + any of aeiou with "ny" + previous char
+            result = Regex.Replace(result, "(N)([aeiouAEIOU])", "$1Y$2"); // Same as above but for upper-case 'N'
+            result = result.Replace("ove", "uv"); // Replace "ove" with "uv"
+
+            return Regex.Replace(result, "!{1,3}", _ => RandomFace());
+        }
+
+        private string RandomFace() => $" {s_faces[_rng.Next(s_faces.Length)]} ";
+    }
+}
