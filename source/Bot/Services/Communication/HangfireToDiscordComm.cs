b24a7ec0ea173ee7d5d2e14444699f6781f138a0
Adam Sears <formlesstree4@tenfoldstudios.com>
2020-11-21 11:42:58 -0600

    HangfireToDiscordComm has been updated to hook into the logging method

--- source/Bot/Services/Communication/HangfireToDiscordComm.cs
+++ source/Bot/Services/Communication/HangfireToDiscordComm.cs
@@ -11,47 +11,65 @@ namespace Bot.Services.Communication
     {
 
         private readonly IServiceProvider services;
-
+        private readonly Func<LogMessage, Task> WriteLog;
         private DiscordSocketClient client;
 
 
-        public HangfireToDiscordComm(IServiceProvider services)
+        public HangfireToDiscordComm(IServiceProvider services, Func<LogMessage, Task> logger)
         {
             this.services = services;
+            this.WriteLog = logger;
             this.client = services.GetRequiredService<DiscordSocketClient>();
         }
 
 
         public async Task InitializeService()
         {
+            Write("Creating initial jobs...");
             this.ScheduleJobs();
             await Task.Yield();
         }
 
         public async Task SendMessageToChannel(ulong channelId, string message)
         {
+            Write($"Fetching Channel (ID {channelId})", LogSeverity.Verbose);
             var c = client.GetChannel(channelId);
             if (c is IMessageChannel mc)
             {
+                Write($"A message is being sent to {mc.Name}");
                 await mc.SendMessageAsync(message);
             }
         }
 
         public async Task SendMessageToUser(ulong userId, string message)
         {
-            var c = client.GetUser("formlesstree4", "2035");
-            if (c is null) {
-                Console.WriteLine($"ERROR GETTING USER DETAILS ({userId})");
+            var dc = client as IDiscordClient;
+            Write($"Fetching User (ID {userId})", LogSeverity.Verbose);
+            var user = await dc.GetUserAsync(userId);
+            if (user is null)
+            {
+                Write($"Unable to retrieve User (ID {userId})", LogSeverity.Error);
                 return;
             }
-            var channel = await c.GetOrCreateDMChannelAsync();
+            var channel = await user.GetOrCreateDMChannelAsync();
+            if (channel is null)
+            {
+                Write($"Failed to create a DM channel for {user.Username}", LogSeverity.Error);
+                return;
+            }
+            Write($"Sending private message to {user.Username}!");
             await channel.SendMessageAsync(message);
         }
 
+        private void Write(string message, LogSeverity severity = LogSeverity.Info)
+        {
+            WriteLog(new LogMessage(severity, nameof(HangfireToDiscordComm), message));
+        }
 
         private void ScheduleJobs()
         {
-            //BackgroundJob.Schedule(() => SendMessageToUser(105497358833336320, "Hey man. I'm alive and well"), TimeSpan.FromSeconds(5));
+            // this job should always happen
+            BackgroundJob.Schedule(() => SendMessageToUser(105497358833336320, "Hey man. I'm alive and well"), TimeSpan.FromSeconds(1));
         }
 
     }
