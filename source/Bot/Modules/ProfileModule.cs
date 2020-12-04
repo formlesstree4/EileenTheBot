de46073bc6d1f03039d5e008ed635d22ff01b48a
Adam Sears <formlesstree4@tenfoldstudios.com>
2020-12-03 18:58:25 -0600

    Profile is now a broker thing

--- source/Bot/Modules/ProfileModule.cs
+++ source/Bot/Modules/ProfileModule.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Bot.Services;
 using Discord.Commands;
@@ -12,18 +13,59 @@ namespace Bot.Modules
 
         [Command("profile")]
         [Summary("Pulls up the User Profile information")]
-        public async Task ProfileAsync()
+        public async Task ProfileAsync(
+            [Summary("The actual command you want to execute for the Profile")]string command = null,
+            [Remainder, Summary("A collection of parameters that are to be passed along for use with the given command")] string[] parameters = null
+        )
         {
             var userId = Context.User.Id;
-            await UserService.CreateUserProfileMessage(userId, Context.Channel);
+            switch (command)
+            {
+                case null:
+                    await UserService.CreateUserProfileMessage(userId, Context.Channel);
+                    break;
+                case "setimg":
+                    var imageUrl = "";
+                    if (parameters?.Length >= 1) imageUrl = parameters[0];
+                    await SetProfileImageAsync(imageUrl);
+                    break;
+                case "rmimg":
+                    await ClearProfileImageAsync();
+                    break;
+            }
         }
 
-        [Command("setimage")]
-        [Summary("Sets up a profile image for you on your Profile page")]
-        public async Task SetProfileImageAsync([Name("Image URL"), Summary("The URL of the image to use on the Profile Page")]string imageUrl)
+        private async Task SetProfileImageAsync(string imageUrl = null)
         {
             var userData = await UserService.GetOrCreateUserData(Context.User.Id);
-            userData.ProfileImage = imageUrl;
+            var fromAttachment = false;
+            if (string.IsNullOrWhiteSpace(imageUrl))
+            {
+                foreach(var attachment in Context.Message.Attachments)
+                {
+                    if (attachment.Height is null) continue;    
+                    imageUrl = attachment.Url;
+                    fromAttachment = true;
+                }
+            }
+            if (string.IsNullOrWhiteSpace(imageUrl))
+            {
+                await Context.Channel.SendMessageAsync("Please either supply an image URL OR attach an image!");
+                return;
+            }
+            var message = "Your profile image has been updated successfully";
+            if (fromAttachment)
+            {
+                message += " from the attachment";
+            }
+            await Context.Channel.SendMessageAsync(message);
+        }
+
+
+        private async Task ClearProfileImageAsync()
+        {
+            var userData = await UserService.GetOrCreateUserData(Context.User.Id);
+            userData.ProfileImage = "";
             await Context.Channel.SendMessageAsync("Your profile image has been updated successfully");
         }
 
