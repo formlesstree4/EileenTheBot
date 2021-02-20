1b912d007362ffb99299925e93210af94ba03883
Adam Sears <asears2@live.com>
2021-02-20 09:53:30 -0600

    Updated the MainAsync method to use a LogAsync() overload and also added in quite a few log statements to more accurately track what Eileen is doing

--- source/Bot/Program.cs
+++ source/Bot/Program.cs
@@ -37,18 +37,25 @@ namespace Bot
 
         public async Task MainAsync()
         {
+            await LogAsync("Entering MainAsync - Good Day", LogSeverity.Verbose);
+            await LogAsync("Initializing Eileen... please wait!");
             currentLogLevel = ParseEnvironmentLogLevel();
+            await LogAsync($"Current Log Level: {currentLogLevel}", LogSeverity.Verbose);
+            await LogAsync("Invoking ConfigureServices()...", LogSeverity.Verbose);
             var services = ConfigureServices();
+            await LogAsync("ConfigureServices() has completed.", LogSeverity.Verbose);
             var client = services.GetRequiredService<DiscordSocketClient>();
             var cts = services.GetRequiredService<CancellationTokenSource>();
-
+            await LogAsync($"Attaching DiscordSocketClient logger to {nameof(LogAsync)}", LogSeverity.Verbose);
             client.Log += LogAsync;
-            await LogAsync(new LogMessage(LogSeverity.Info, nameof(MainAsync), "Initializing Services..."));
+            await LogAsync($"Attaching the CommandService logger to {nameof(LogAsync)}", LogSeverity.Verbose);
             services.GetRequiredService<CommandService>().Log += LogAsync;
+            await LogAsync("Most Discord related functionality has been setup - moving on...");
+            await LogAsync($"Initializing RavenDB connectivity...");
             var ravenService = services.GetRequiredService<RavenDatabaseService>();
             await ravenService.InitializeService();
 
-            // Setup hangfire real quick...
+            await LogAsync("Beginning Hangfire initialization");
             var configuration = ravenService.Configuration;
             var hangfireActivator = new SpecialActivator(services);
             var connString = $"User ID={configuration.RelationalDatabase.Username};Password={configuration.RelationalDatabase.Password};Host={configuration.RelationalDatabase.Hostname};Port=5432;Database={configuration.RelationalDatabase.Database};Pooling=true;";
@@ -58,8 +65,9 @@ namespace Bot
                 QueuePollInterval = TimeSpan.FromSeconds(5),
                 InvisibilityTimeout = TimeSpan.FromSeconds(5)
             });
-            GlobalConfiguration.Configuration.UseColouredConsoleLogProvider(Hangfire.Logging.LogLevel.Info);
+            GlobalConfiguration.Configuration.UseColouredConsoleLogProvider(ParseEnvironmentLogLevelForHangfire());
             GlobalConfiguration.Configuration.UseActivator(hangfireActivator);
+            await LogAsync("Hangfire configuration has been established, setting up Job Server...");
 
             var bjs = new BackgroundJobServer(
                 new BackgroundJobServerOptions
@@ -79,12 +87,17 @@ namespace Bot
                     ServerName = "Eileen-Host",
                 }, JobStorage.Current);
 
+            await LogAsync("Job Server has been setup and configured.");
+            await LogAsync("Initializing the remaining Eileen services...");
+
             await InitializeServices(services);
+            await LogAsync("All services initialized, logging into Discord");
             await client.LoginAsync(TokenType.Bot, configuration.DiscordToken);
             await client.StartAsync();
             await client.SetStatusAsync(UserStatus.Online);
             await client.SetGameAsync(name: "A bot made by formlesstree4", streamUrl: null, type: ActivityType.CustomStatus);
-
+            await LogAsync("Connected to Discord without error!");
+            await LogAsync("Creating WebUI for Hangfire's Dashboard");
             var ui = WebHost
                 .CreateDefaultBuilder()
                 .UseSetting("connection-string", connString)
@@ -95,21 +108,23 @@ namespace Bot
 #pragma warning disable CS4014
             ui.RunAsync();
 #pragma warning restore CS4014
-
+            await LogAsync("The Hangfire Dashboard has been initialized on port 5000 listening to all addresses");
             try
             {
                 await Task.Delay(Timeout.Infinite, cts.Token);
             }
             catch (TaskCanceledException)
             {
-                Console.WriteLine("Shutdown command has been received!");
+                await LogAsync("The cancellation token has been canceled. Eileen is now shutting down");
             }
-            Console.WriteLine("Dumping Markov history into the DB");
+            await LogAsync("Shutting down Hangfire...");
             bjs.Dispose();
+            await LogAsync("Saving data to RavenDB...");
             await services.GetRequiredService<UserService>().SaveServiceAsync();
             await services.GetRequiredService<MarkovService>().SaveServiceAsync();
             await services.GetRequiredService<DungeoneeringMainService>().SaveServiceAsync();
             await services.GetRequiredService<ServerConfigurationService>().SaveServiceAsync();
+            await LogAsync("Tasks all completed - Going offline");
         }
 
         private LogSeverity ParseEnvironmentLogLevel()
@@ -135,6 +150,33 @@ namespace Bot
                 case "DEBUG":
                 case "5":
                     return LogSeverity.Debug;
+            }
+            throw new InvalidOperationException("Somehow failed to parse the logging level");
+        }
+
+        private Hangfire.Logging.LogLevel ParseEnvironmentLogLevelForHangfire()
+        {
+            switch(Environment.GetEnvironmentVariable("LogLevel")?.ToUpperInvariant())
+            {
+                case "CRITICAL":
+                case "0":
+                    return Hangfire.Logging.LogLevel.Fatal;
+                case "ERROR":
+                case "1":
+                    return Hangfire.Logging.LogLevel.Error;
+                case "WARNING":
+                case "2":
+                    return Hangfire.Logging.LogLevel.Warn;
+                case "INFO":
+                case "3":
+                case null:
+                    return Hangfire.Logging.LogLevel.Info;
+                case "VERBOSE":
+                case "4":
+                    return Hangfire.Logging.LogLevel.Trace;
+                case "DEBUG":
+                case "5":
+                    return Hangfire.Logging.LogLevel.Debug;
             }
             throw new InvalidOperationException("Somehow failed to parse the logging level");
         }
@@ -196,6 +238,9 @@ namespace Bot
             .BuildServiceProvider();
 
 
+
+        private Task LogAsync(string message, LogSeverity severity = LogSeverity.Info)
+            => LogAsync(new LogMessage(severity, "Eileen", message));
 
         private Task LogAsync(LogMessage log)
         {
