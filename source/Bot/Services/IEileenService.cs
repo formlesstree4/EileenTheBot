2125ff1fa4c1974eb7e595d8e3ffda130656bd00
Adam Sears <asears2@live.com>
2021-02-20 10:34:58 -0600

    Created IEileenService to help discoverability of services

---
+++ source/Bot/Services/IEileenService.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+
+namespace Bot.Services
+{
+
+    /// <summary>
+    /// Definition of a custom provided service for Eileen. Also used to help facilitate auto-discovery and registration of said services.
+    /// </summary>
+    public interface IEileenService
+    {
+
+        /// <summary>
+        /// Initializes the given service
+        /// </summary>
+        /// <returns>A Task that, when completed, will indicate the service has been initialized</returns>
+        Task InitializeService() => Task.CompletedTask;
+
+        /// <summary>
+        /// Saves any persistent data for the service
+        /// </summary>
+        /// <returns>A Task that, when completed, will indicate the service has saved all persistent data to the backing data store</returns>
+        Task SaveServiceAsync() => Task.CompletedTask;
+
+        /// <summary>
+        /// Loads (or reloads) any persistent data for the service
+        /// </summary>
+        /// <returns>A Task that, when completed, will indicate the service has (re)loaded all persistent data from the backing data store</returns>
+        Task LoadServiceAsync() => Task.CompletedTask;
+
+        /// <summary>
+        /// Determines whether or not this service should be auto-initialized by the runtime
+        /// </summary>
+        /// <returns>bool</returns>
+        bool AutoInitialize() => true;
+        
+    }
+
+
+}
