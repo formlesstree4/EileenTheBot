bad639a4a01f2cb91df6aa92d5da6e87bbf48895
Adam Sears <asears2@live.com>
2021-02-20 12:05:15 -0600

    Allows for a service to specify 'how' it can be registered (whether it is Transient, Singleton, or Scoped)

---
+++ source/Bot/Models/ServiceType.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Bot.Models
+{
+    public enum ServiceType
+    {
+        Singleton = 0,
+        Transient = 1,
+        Scoped = 2
+    }
+
+    public sealed class ServiceTypeAttribute : Attribute
+    {
+        
+        public ServiceType ServiceType { get; init ;}
+
+        public ServiceTypeAttribute(ServiceType type) => this.ServiceType = type;
+
+    }
+
+}
