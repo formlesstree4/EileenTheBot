025789fb95daeff9bc7396f85508c3748d5210cf
Adam Sears <asears2@live.com>
2022-08-28 01:45:09 -0500

    removed GitHashAttribute. It's not working anyway

--- source/Bot/Program.cs
+++ source/Bot/Program.cs
@@ -47,7 +47,6 @@ namespace Bot
             logger.LogTrace("Entering MainAsync - Good Day");
             logger.LogTrace("Initializing Eileen... please wait!");
             logger.LogTrace("Current Log Level: {currentLogLevel}", currentLogLevel);
-            logger.LogInformation("Eileen - Commit Hash {hash}", Assembly.GetEntryAssembly().GetCustomAttribute<GitHashAttribute>().Hash);
 
             var client = services.GetRequiredService<DiscordSocketClient>();
             var cts = services.GetRequiredService<CancellationTokenSource>();
