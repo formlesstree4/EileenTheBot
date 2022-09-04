f071af059ff6e4f3539fa797b1ce1f967debdbe3
Adam Sears <asears2@live.com>
2022-09-04 02:35:42 -0500

    Re-implemented the blackjack module commands and also did some lazy enforcement on preventing your bid from getting into the negatives

--- source/Bot/Modules/BlackJackModule.cs
+++ source/Bot/Modules/BlackJackModule.cs
@@ -2,6 +2,7 @@ using Bot.Services;
 using Bot.Services.BlackJack;
 using Discord;
 using Discord.Interactions;
+using System;
 using System.Threading.Tasks;
 
 namespace Bot.Modules
@@ -10,17 +11,20 @@ namespace Bot.Modules
     public sealed class BlackJackModule : InteractionModuleBase
     {
         private readonly BlackJackService blackJackService;
+        private readonly BlackJackTableRunnerService blackJackTableRunnerService;
         private readonly CurrencyService currencyService;
         private readonly InteractionHandlingService interactionHandlingService;
         private readonly UserService userService;
 
         public BlackJackModule(
             BlackJackService blackJackService,
+            BlackJackTableRunnerService blackJackTableRunnerService,
             CurrencyService currencyService,
             InteractionHandlingService interactionHandlingService,
             UserService userService)
         {
             this.blackJackService = blackJackService ?? throw new System.ArgumentNullException(nameof(blackJackService));
+            this.blackJackTableRunnerService = blackJackTableRunnerService ?? throw new System.ArgumentNullException(nameof(blackJackTableRunnerService));
             this.currencyService = currencyService ?? throw new System.ArgumentNullException(nameof(currencyService));
             this.interactionHandlingService = interactionHandlingService ?? throw new System.ArgumentNullException(nameof(interactionHandlingService));
             this.userService = userService ?? throw new System.ArgumentNullException(nameof(userService));
@@ -36,19 +40,61 @@ namespace Bot.Modules
         [SlashCommand("join", "Joins a BlackJack table")]
         public async Task Join()
         {
-            await RespondAsync("Too lazy to implement, sorry", ephemeral: true);
+            if (Context.Channel is IThreadChannel tc)
+            {
+                var game = blackJackService.FindBlackJackGame(tc);
+                await blackJackTableRunnerService.AddPlayerSafelyToTable(game, Context.User);
+                await RespondAsync($"Welcome to the table {Context.User.Mention}! Here are a few preset Bid buttons to interact with. Alternately you set your Bid directly with `/blackjack bid <amount>` to set your Bid to any number",
+                    ephemeral: true, components: BlackJackTableRunnerService.GetBidButtonComponents(tc.Id).Build());
+            }
+            else
+            {
+                await DeferAsync();
+            }
         }
 
         [SlashCommand("leave", "Leaves a BlackJack table")]
         public async Task Leave()
         {
-            await RespondAsync("Too lazy to implement, sorry", ephemeral: true);
+            if (Context.Channel is IThreadChannel tc)
+            {
+                var game = blackJackService.FindBlackJackGame(tc);
+                if (blackJackTableRunnerService.RemovePlayerSafelyFromTable(game, Context.User))
+                {
+                    await RespondAsync($"You have been removed from the table! Thank you for playing.", ephemeral: true);
+                }
+                else
+                {
+                    await RespondAsync("Couldn't remove you from the table. Have you already left?");
+                }
+            }
+            else
+            {
+                await DeferAsync();
+            }
+
         }
 
         [SlashCommand("bet", "Gets or sets your static Bet for the room")]
         public async Task Bet(ulong? amount = null)
         {
-            await RespondAsync("Too lazy to implement, sorry", ephemeral: true);
+            if (Context.Channel is IThreadChannel tc)
+            {
+                var currentTable = blackJackService.FindBlackJackGame(tc);
+                if (!currentTable.CanPlayerAlterBet(Context.User.Id))
+                {
+                    await RespondAsync("Sorry, you can't change your bet right now!", ephemeral: true);
+                    return;
+                }
+                var player = currentTable.FindPlayer(Context.User.Id);
+                if (player is null)
+                {
+                    await RespondAsync("Sorry, I couldn't find you at the table... which is strange. You should report this as a bug to the maintainer", ephemeral: true);
+                    return;
+                }
+                player.CurrentBet = Math.Max(0, amount ?? 0);
+                await RespondAsync($"Your bet has now been set to {player.CurrentBet}", ephemeral: true);
+            }
         }
 
 
