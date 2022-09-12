8371db9f801ac15c90162e79df5db2462b38cd24
Adam Sears <asears2@live.com>
2022-09-11 22:32:03 -0500

    quinNNNNNNNNNNNNNN

--- source/Bot/Modules/CoolSwiftModule.cs
+++ source/Bot/Modules/CoolSwiftModule.cs
@@ -1,21 +1,33 @@
+using Bot.Preconditions;
+using Discord;
 using Discord.Interactions;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Bot.Modules
 {
 
-    [Group("coolswift", "( ͡° ᴥ ͡°)")]
+    [Group("cs", "( ͡° ᴥ ͡°)"), CoolSwiftPrecondition]
     public sealed class CoolSwiftModule : InteractionModuleBase
     {
+        private readonly Random random;
+
+        public CoolSwiftModule(Random random)
+        {
+            this.random = random;
+        }
 
 
-
-        [SlashCommand("quinn", "")]
+        [SlashCommand("quinn", "QUINnNnNnnNNN!~")]
         public async Task Quinn()
         {
-
-
-
+            var quinnFiles = Directory.GetFiles(Path.Combine("Resources", "Quinn"));
+            var fileToSend = quinnFiles[random.Next(quinnFiles.Length - 1)];
+            using var reader = new FileStream(fileToSend, FileMode.Open, FileAccess.Read);
+            var attachment = new FileAttachment(reader, Path.GetFileName(fileToSend));
+            await RespondWithFileAsync(attachment);
+            reader.Close();
         }
 
 
---
+++ source/Bot/Preconditions/CoolSwiftPrecondition.cs
@@ -0,0 +1,20 @@
+using Discord;
+using Discord.Interactions;
+using System;
+using System.Threading.Tasks;
+
+namespace Bot.Preconditions
+{
+    public sealed class CoolSwiftPrecondition : PreconditionAttribute
+    {
+
+        private const ulong coolswiftUserId = 143551309776289792;
+
+        public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+        {
+            return Task.FromResult(context.User.Id == coolswiftUserId ?
+                PreconditionResult.FromSuccess() :
+                PreconditionResult.FromError("You are not allowed to perform this action"));
+        }
+    }
+}
