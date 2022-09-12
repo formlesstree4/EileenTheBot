df27bd00f3d1d67c60408f95d45fdd55eb6eb22e
Adam Sears <asears2@live.com>
2022-09-11 22:45:37 -0500

    more QUIIIIIIIIInNnNNnnnnNNn

--- source/Bot/Modules/CoolSwiftModule.cs
+++ source/Bot/Modules/CoolSwiftModule.cs
@@ -19,14 +19,14 @@ namespace Bot.Modules
         }
 
 
-        [SlashCommand("quinn", "QUINnNnNnnNNN!~")]
+        [SlashCommand("quinn", "ᕙ(▀̿ĺ̯▀̿ ̿)ᕗ QUINnNnNnnNNN!~")]
         public async Task Quinn()
         {
             var quinnFiles = Directory.GetFiles(Path.Combine("Resources", "Quinn"));
             var fileToSend = quinnFiles[random.Next(quinnFiles.Length - 1)];
             using var reader = new FileStream(fileToSend, FileMode.Open, FileAccess.Read);
             var attachment = new FileAttachment(reader, Path.GetFileName(fileToSend));
-            await RespondWithFileAsync(attachment);
+            await RespondWithFileAsync(attachment, ephemeral: true);
             reader.Close();
         }
 
